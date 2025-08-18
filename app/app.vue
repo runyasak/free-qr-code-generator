51fb22c6439f494c7d70a3a21c5474f5922f4263
Runyasak Chaengnaimuang <runyasak.c@gmail.com>
2025-08-18 23:21:05 +0700

    feat: integrate QR code generation functionality, add new dependency for qr-code-styling, and implement user input for dynamic QR code updates.

--- app/app.vue
+++ app/app.vue
@@ -1,5 +1,52 @@
+<script setup lang="ts">
+import QRCodeStyling from 'qr-code-styling'
+
+const qrData = ref('')
+
+const qrCode = ref<QRCodeStyling | null>(null)
+
+const exampleData = 'https://example.com'
+
+onMounted(() => {
+  qrCode.value = new QRCodeStyling({
+    width: 1000,
+    height: 1000,
+    type: 'svg',
+    data: exampleData,
+    dotsOptions: {
+      type: 'rounded',
+    },
+    cornersSquareOptions: {
+      type: 'rounded',
+    },
+  })
+
+  qrCode.value?.append(document.getElementById('qr-code-canvas')!)
+})
+
+watch(qrData, (newVal) => {
+  if (newVal) {
+    qrCode.value?.update({
+      data: newVal,
+    })
+  }
+  else {
+    qrCode.value?.update({
+      data: exampleData,
+    })
+  }
+})
+
+function downloadQRCode() {
+  qrCode.value?.download({
+    name: 'qr-code',
+  })
+}
+</script>
+
 <template>
   <div
+    h-screen
     p-12
     mx="auto"
     container
@@ -7,38 +54,61 @@
     flex
     flex-col
     gap-8
+    justify-center
   >
-    <h1
+    <div
       text-center
       text-white
       text-5xl
     >
-      FREE QR Generator
-    </h1>
+      สร้าง QR Code ฟรี
+    </div>
 
     <p
       text-main-content
       text-center
       uppercase
     >
-      Enter Text or URL
+      ใส่ URL หรือข้อความที่ต้องการ
     </p>
 
     <input
+      v-model="qrData"
       p-2
       bg="#161618"
       border="gray-500/50"
       border-solid
       hover:border-primary
       focus:border-primary
-      transition-colors
-      transition-duration-300
-      transition-ease-in-out
+      my-transition
       focus-visible:outline-none
       rounded
       color-white
       type="text"
-      placeholder="https://example.com or any text..."
+      :placeholder="exampleData"
     >
+    <div
+      id="qr-code-canvas"
+      m-auto
+      flex-1
+      overflow-y-hidden
+      class="[&_svg]:h-full"
+    />
+
+    <button
+      bg-primary
+      px-6
+      py-4
+      rounded
+      border-0
+      uppercase
+      font-bold
+      my-transition
+      :class="qrData ? 'cursor-pointer' : 'cursor-not-allowed !bg-gray-500/50'"
+      :disabled="!qrData"
+      @click="downloadQRCode"
+    >
+      ดาวน์โหลด QR Code
+    </button>
   </div>
 </template>
--- uno.config.ts
+++ uno.config.ts
@@ -2,6 +2,9 @@ import { defineConfig, presetAttributify, presetWind3 } from 'unocss'
 
 export default defineConfig({
   presets: [presetAttributify(), presetWind3()],
+  shortcuts: [
+    ['my-transition', 'transition-colors transition-duration-300 transition-ease-in-out'],
+  ],
   theme: {
     colors: {
       'main-bg': 'var(--main-bg)',
