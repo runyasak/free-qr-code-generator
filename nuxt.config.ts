13fb419555e1c8f96887cad6f99f945bbb34eedd
Runyasak Chaengnaimuang <runyasak.c@gmail.com>
2025-08-19 00:20:17 +0700

    feat: integrate nuxt-gtag module for Google Tag Manager support, removing inline scripts and noscript iframe from app.vue for cleaner implementation

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint', '@unocss/nuxt', '@nuxt/fonts', '@nuxthub/core'],
+  modules: ['@nuxt/eslint', '@unocss/nuxt', '@nuxt/fonts', '@nuxthub/core', 'nuxt-gtag'],
   css: ['~/assets/css/reset.css', '~/assets/css/main.css'],
   eslint: {
     config: {
@@ -59,20 +59,9 @@ export default defineNuxtConfig({
         { rel: 'canonical', href: 'https://free-qr-code-generator.nuxt.dev' }, // Replace with your actual domain
         { rel: 'alternate', hreflang: 'th', href: 'https://free-qr-code-generator.nuxt.dev' },
       ],
-      script: [
-        {
-          innerHTML: `
-            <!-- Google Tag Manager -->
-(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-WWSQN9Z3');
-<!-- End Google Tag Manager -->
-          `,
-          type: 'text/javascript'
-        }
-      ]
     }
+  },
+  gtag: {
+    id: 'GTM-WWSQN9Z3'
   }
 })
