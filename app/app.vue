a98681af129e8b96127e2a71999d8ac00427a7af
Runyasak Chaengnaimuang <runyasak.c@gmail.com>
2025-08-25 23:19:52 +0700

    fix: adjust layout

--- app/app.vue
+++ app/app.vue
@@ -146,12 +146,16 @@ useHead({
     mx="auto"
     container
     max-w-3xl
-    flex
-    flex-col
-    gap-8
+    flex="~ col"
+    gap="4 md:8"
     justify-center
   >
-    <div class="text-center flex flex-col gap-2">
+    <div
+      text-center
+      flex
+      flex-col
+      gap-2
+    >
       <div
         text-white
         text-4xl
@@ -168,8 +172,11 @@ useHead({
         {{ $t('subtitle') }}
       </p>
     </div>
-
-    <div class="flex flex-col gap-2">
+    <div
+      flex
+      flex-col
+      gap-2
+    >
       <p
         text-main-content
         md:text-xl
@@ -193,62 +200,77 @@ useHead({
         type="text"
         :placeholder="$t('placeholder')"
       >
-
-      <button
-        bg-transparent
-        border-solid
-        px-3
-        py-2
-        rounded-md
-        relative
-        flex
-        items-center
-        gap-2
-        md:text-xl
-        my-transition
-        :disabled="!hasQrCode"
-        :class="
-          !hasQrCode
-            ? 'cursor-not-allowed text-gray-500 border-gray-500/50'
-            : 'cursor-pointer hover:border-primary text-white border-white hover:text-primary'"
-        @click="logoImageInput?.click()"
-      >
-        <svg
-          min-w="28px"
-          xmlns="http://www.w3.org/2000/svg"
-          width="28px"
-          height="28px"
-          viewBox="0 0 24 24"
-        ><path
-          fill="currentColor"
-          d="m9 13l3-4l3 4.5V12h4V5c0-1.103-.897-2-2-2H4c-1.103 0-2 .897-2 2v12c0 1.103.897 2 2 2h8v-4H5l3-4z"
-        /><path
-          fill="currentColor"
-          d="M19 14h-2v3h-3v2h3v3h2v-3h3v-2h-3z"
-        /></svg>
-        <span line-clamp-1>
-          {{ logoImageFileName || $t('addLogoImage') }}
-        </span>
-      </button>
     </div>
-
     <div
-      rounded-md
-      border-1
-      border-solid
-      border-white
-      flex-1
-      mx-auto
-      aspect-square
-      p-1
-      overflow-hidden
+      flex="~ col md:flex-row"
+      gap="4 md:8"
+      md="flex-row gap-8"
     >
       <div
-        id="qr-code-canvas"
-        overflow-y-hidden
+        flex="~ col md:1"
+        gap="4 md:8"
+      >
+        <div
+          flex="~ col"
+          gap-2
+        >
+          <button
+            bg-transparent
+            border-solid
+            px-3
+            py-2
+            rounded-md
+            relative
+            flex
+            items-center
+            gap-2
+            md:text-xl
+            my-transition
+            :disabled="!hasQrCode"
+            :class="
+              !hasQrCode
+                ? 'cursor-not-allowed text-gray-500 border-gray-500/50'
+                : 'cursor-pointer hover:border-primary text-white border-white hover:text-primary'"
+            @click="logoImageInput?.click()"
+          >
+            <svg
+              min-w="28px"
+              xmlns="http://www.w3.org/2000/svg"
+              width="28px"
+              height="28px"
+              viewBox="0 0 24 24"
+            ><path
+              fill="currentColor"
+              d="m9 13l3-4l3 4.5V12h4V5c0-1.103-.897-2-2-2H4c-1.103 0-2 .897-2 2v12c0 1.103.897 2 2 2h8v-4H5l3-4z"
+            /><path
+              fill="currentColor"
+              d="M19 14h-2v3h-3v2h3v3h2v-3h3v-2h-3z"
+            /></svg>
+            <span line-clamp-1>
+              {{ logoImageFileName || $t('addLogoImage') }}
+            </span>
+          </button>
+        </div>
+      </div>
+      <div
+        md:flex-1
+        md:mt-0
         rounded-md
-        class="[&_svg]:h-full"
-      />
+        border-1
+        border-solid
+        border-white
+        mx-auto
+        aspect-square
+        p-1
+        overflow-hidden
+      >
+        <div
+          id="qr-code-canvas"
+          overflow-y-hidden
+          rounded-md
+          class="[&_svg]:h-full"
+        />
+      </div>
     </div>
 
     <button
